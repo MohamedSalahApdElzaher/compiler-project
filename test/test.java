96d03d64f187ec55b4f920f3f7f1518657d5965d
salmarefaie <76884936+salmarefaie@users.noreply.github.com>
2022-04-27 20:51:42 +0200

    Create test.java

---
+++ test/test.java
@@ -0,0 +1,38 @@
+package test;
+
+public class test {
+    public static void main(String[] args) {
+        int x = 10 ;
+        int y = 20;
+        if (y == 6 )
+        {
+            x = 0;
+        }else if( x < 4)
+        {
+            x = 1 ;
+        }
+        else if( x > 15)
+        {
+            x = 2 ;
+        }
+        else {
+            x=3;
+        }
+    }
+}
+/*
+public class test {
+    public static void main(String[] args) {
+        int x = 2 ;
+        int y = 5;
+        if (x > 2){
+            y = 8;
+        }
+        else{
+            y = 1;
+        }
+        System.out.println(y);
+    }
+}
+
+ */
