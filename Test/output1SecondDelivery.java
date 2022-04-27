90b8a99a7e52763ce3cb0a2a518257d426a4e231
salmarefaie <76884936+salmarefaie@users.noreply.github.com>
2022-04-27 21:06:29 +0200

    Create output1SecondDelivery.java

---
+++ Test/output1SecondDelivery.java
@@ -0,0 +1,26 @@
+package output;
+
+public class output {
+    public static void main(String[] args) {
+        int x = 10 ;
+        int y = 20;
+        if (y == 6 )
+        {
+		System.out.println("Block #0");
+            x = 0;
+        }else if( x < 4)
+        {
+		System.out.println("Block #1");
+            x = 1 ;
+        }
+        else if( x > 15)
+        {
+		System.out.println("Block #2");
+            x = 2 ;
+        }
+        else {
+		System.out.println("Block #3");
+            x=3;
+        }
+    }
+}
