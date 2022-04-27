b4ba3c8fd19abf20014a909de35c25431890c8fd
salmarefaie <76884936+salmarefaie@users.noreply.github.com>
2022-04-27 21:08:17 +0200

    Create test2SecondDelivery.java

---
+++ Test/test2SecondDelivery.java
@@ -0,0 +1,14 @@
+package test;
+public class test {
+    public static void main(String[] args) {
+        int x = 2 ;
+        int y = 5;
+        if (x > 2){
+            y = 8;
+        }
+        else{
+            y = 1;
+        }
+        System.out.println(y);
+    }
+}
