63f0e9fcadd2a51d811eb9f566a637a3877ffbd8
salmarefaie <76884936+salmarefaie@users.noreply.github.com>
2022-05-05 21:28:07 +0200

    Add files via upload

---
+++ test/test1.java
@@ -0,0 +1,17 @@
+package test;
+import java.io.PrintWriter;
+public class test1 {
+    public static void main(String args[]) throws Exception
+    {
+             int x = 10 ;
+        int y = 6;
+        if (y == 6 ){
+            x = 0;}
+        else if( x < 4){
+            x = 1 ;}
+        else if( x > 15){
+            x = 2 ;}
+        else{
+            x=3;}
+    }
+}
---
+++ test/test3.java
@@ -0,0 +1,18 @@
+package test;
+import java.io.PrintWriter;
+
+public class test3 {
+    public static void main(String args[]) throws Exception
+    {
+        int x = 1, sum = 0;
+        while (x <= 10) {
+            if (x<4)
+                System.out.println("y");
+            else{
+                System.out.println("yyyyy");
+            }
+            x ++;
+        }
+
+    }
+}
